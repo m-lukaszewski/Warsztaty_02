dda641076e322d28244ffeee033d62e4bf8fe561
Marcin Łukaszewski <lukaszewski.marcin@gazeta.pl>
2019-03-09 13:28:35 +0100

    Group class created

---
+++ src/pl/coderslab/model/Group.java
@@ -0,0 +1,38 @@
+package pl.coderslab.model;
+
+public class Group {
+
+    private int id;
+    private String name;
+
+
+    public Group() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Group(String name) {
+        this.name = name;
+
+    }
+
+
+
+
+}
+
